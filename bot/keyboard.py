27ec3f59ae698a123ae8fbfeddd5dea9ff1e758b
BIRZHAN <birzhan981@gmail.com>
2024-08-18 08:56:06 +0500

    Dynamic display of company names on buttons has been added to the start function, with automatic data import from Google Sheets.

--- bot/keyboard.py
+++ bot/keyboard.py
@@ -1,6 +1,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 import logging
+from charts import get_company_sheet_map
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -18,14 +19,18 @@ async def start(update: Update, context: CallbackContext):
         update (Update): Объект обновления от Telegram.
         context (CallbackContext): Контекст обработки запроса.
     """
+    # Получение маппинга компаний и листов
+    company_sheet_map = get_company_sheet_map()
+
     # Создание клавиатуры с кнопками для выбора компании
     keyboard = [
-        [InlineKeyboardButton("Onyx Corp.", callback_data='company_1')],
-        [InlineKeyboardButton("Parlamentarioum LTD.", callback_data='company_2')],
-        [InlineKeyboardButton("Michelangello LLP.", callback_data='company_3')],
-        [InlineKeyboardButton("Almaty Cube LTD.", callback_data='company_4')],
+        [InlineKeyboardButton(company_name, callback_data=company_id)]
+        for company_id, company_name in company_sheet_map.items()
     ]
+
+    # Создание разметки для кнопок
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     # Отправка сообщения с клавиатурой пользователю
     await update.message.reply_text('Выберите компанию:', reply_markup=reply_markup)
+
